039076e5663a028e78e976a38fad3ed5372b2bab
Ju Josh Kim <jktpk91@gmail.com>
2021-05-29 18:44:32 -0700

    update models

--- posts_api/models.py
+++ posts_api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    author = models.IntegerField()
-    image = models.CharField(max_length=128)
-    text = models.CharField(max_length=140)
+    author = models.IntegerField(blank=False)
+    image = models.CharField(max_length=256, blank=True, null=True)
+    text = models.CharField(max_length=256, blank=False)
     liked_by = ArrayField(models.IntegerField())
     comments = ArrayField(models.IntegerField())
