a0839e85d113683b1199387c846436379cbc1006
Ju Josh Kim <jktpk91@gmail.com>
2021-05-31 09:33:47 -0700

    update serializers for all models

--- posts_api/serializers.py
+++ posts_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'user', 'image', 'text', 'liked_by', 'comments')
+        fields = ('id', 'user', 'image', 'text', 'liked_by')
