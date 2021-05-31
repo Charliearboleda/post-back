57cd2d8a7b45683d56fd09556bdc4bdd8528a7d2
Ju Josh Kim <jktpk91@gmail.com>
2021-05-30 21:15:19 -0700

    update serializer for users_api

--- users_api/serializers.py
+++ users_api/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password', 'posts', 'following', 'followers')
+        fields = ('id', 'email', 'displayName', 'tagLine', 'posts', 'following', 'followers')
